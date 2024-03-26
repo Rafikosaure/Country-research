ae4154946a49f7476394d537119e874a3498ddf1
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-03-26 12:31:28 +0100

    Mise en place du code html et css du site.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 
 function App() {
+
+
+
+
   return (
     <div className="App">
-      
+      <form>
+        <input className='country-bar' type="text" name="country-researcher" id="" />
+        <input className='submit-btn' type="submit" value="Lancer la recherche" />
+      </form>
+      <div className='div-result'>
+        <h2>Résultats de recherche</h2>
+        <p></p>
+      </div>
     </div>
   );
 }
